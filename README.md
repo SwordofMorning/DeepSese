119d061d54063b861aa07820862b4cecddd2a697
SwordofMorning <969211735@qq.com>
2026-01-18 23:29:38 +0800

    Perf: 更新README，重新梳理Prompt

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # DeepSese
 
-Model List:
+This project is based on Stable Diffusion and aims to generate Asian cosplay-style portraits with extremely realistic features, complex lighting and shadows, and natural skin textures.
 
-```
-hardcoreAsianCosplay_ilV11
-```
+## Dependents
+
+1. Python 3.10+
+2. CUDA Capable GPU
+2. PyTorch 2.0+
+
+## Modules
+
+1. hardcoreAsianCosplay_ilV11
--- src/prompt.py
+++ src/prompt.py
@@ -1,27 +1,36 @@
 # src/prompt.py
 
+# --- Note ---
+# The order of the Prompts affects the output of SD (Simultaneous Visual Representation).
+#
+# For example: 
+#
+# Moving the Camera section forward, will result in a more professional-looking output; 
+# moving the Light section forward, will emphasize lighting and shadow effects.
+# --- Note ---
+
 ##### Section I : Text to Image Prompt #####
 
 # Step 1 : Positive
 PROMPT_TEXT = (
-# Camera 
-    "fujifilm xt4, film grain, high iso, raw style, gravure, "
-# Lighting
-    "chiaroscuro, hard shadows, dimly lit, "
 # Actor
     # Basic
-    "Japanese cosplayer girl, "
+    "Japanese cosplayer girl , "
     # Detail
-    "Raiden Shogun Cosplay, purple braided hair, kimono, cleavage, "
+    "Raiden Shogun Cosplay, kimono, cleavage, "
     # Makeup
     "soft makeup, realistic eyes, "
     "sweat, mole, "
     # Action
-    "looking at viewer, selfie, "
+    "looking at viewer, selfie angle, flash photography, "
     # Background
-    "bedroom, "
-# Real Config
-    "textured skin, visible pores, "
+    "bedroom, messy bed sheet,"
+# Skin
+    "natural skin texture, visible pores, slight skin imperfections, "
+# Camera
+    "fujifilm xt4, film grain, high iso, raw style, gravure"
+# Light
+    "chiaroscuro, hard shadows, dimly lit, "
 )
 
 # Step 2 : Negative
@@ -29,7 +38,7 @@ NEGATIVE_PROMPT_TEXT = (
 # CG & Artificial
     "anime, cartoon, 3d, 3d render, cgm, illustration, painting, drawing, art, "
     "doll, plastic, wax figure, action figure, "
-# Lighting
+# Light
     "flat lighting, soft lighting, even lighting, bright, overexposed, studio lighting, "
 # Skin flaws
     "perfect skin, airbrushed, photoshop, retouched, smooth skin, oil skin, "
